527eec94c506fc20f68407b0639d1b6c3be8426f
Maniackaa <maniac_kaa@mail.ru>
2024-02-07 12:18:23 +0700

    Ver 1 ocr

--- backend_deposit/ocr/admin.py
+++ backend_deposit/ocr/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from ocr.models import ScreenResponse
+from ocr.models import ScreenResponse, ScreenResponsePart
 
 
 class ScreenResponseAdmin(admin.ModelAdmin):
@@ -10,4 +10,13 @@ class ScreenResponseAdmin(admin.ModelAdmin):
     # list_display_links = ('id', 'created', 'title', )
 
 
+class ScreenResponsePartAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'black', 'white', 'recipient', 'sender', 'transaction'
+    )
+    list_filter = ('screen', 'black')
+    list_per_page = 1000
+
+
 admin.site.register(ScreenResponse, ScreenResponseAdmin)
+admin.site.register(ScreenResponsePart, ScreenResponsePartAdmin)
