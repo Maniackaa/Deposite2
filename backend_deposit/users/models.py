8967798a9fe9c2123eda0243bb4b57c94d6bda4c
Maniackaa <maniac_kaa@mail.ru>
2024-03-19 08:37:12 +0700

    add is_active iser

--- backend_deposit/users/models.py
+++ backend_deposit/users/models.py
@@ -57,6 +57,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.username
