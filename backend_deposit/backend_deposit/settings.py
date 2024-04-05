7acbdb25d9b7f874a3ca397ce460b95942494c01
Maniackaa <maniac_kaa@mail.ru>
2024-04-05 23:58:24 +0700

    test

--- backend_deposit/backend_deposit/settings.py
+++ backend_deposit/backend_deposit/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-    # 'django_structlog.middlewares.RequestMiddleware',
+    'django_structlog.middlewares.RequestMiddleware',
     'better_exceptions.integrations.django.BetterExceptionsMiddleware',
 ]
 
