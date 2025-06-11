baa1137fa9b586691076e705db9bf1ba5ac9ef71
Maniackaa <maniac_kaa@mail.ru>
2025-06-11 11:28:55 +0700

    fix

--- gpt_test.py
+++ gpt_test.py
@@ -1,43 +1,33 @@
 import base64
-import os
-import re
-from pathlib import Path
+import json
 
+import httpx
 import requests
-import structlog
 from PIL import Image
 from io import BytesIO
+from pathlib import Path
+import os
 import openai
-from django.conf import settings
+from dotenv import load_dotenv
 
-from core.global_func import Timer
+load_dotenv()
 
-logger = structlog.get_logger('deposite')
+
+client = openai.OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+)
 
 
 def encode_image(image_path):
     with Image.open(image_path) as img:
-        if img.mode in ("RGBA", "LA"):
-            background = Image.new("RGB", img.size, (255, 255, 255))
-            background.paste(img, mask=img.split()[-1])  # Альфа как маска
-            img = background
-        elif img.mode != "RGB":
-            img = img.convert("RGB")
         buffered = BytesIO()
         img.save(buffered, format="JPEG")
         return base64.b64encode(buffered.getvalue()).decode("utf-8")
 
 
-def extract_json(text):
-    # Убираем обертку вида ```json ... ```
-    text = re.sub(r"^```json\s*", "", text.strip(), flags=re.IGNORECASE)
-    text = re.sub(r"^```", "", text.strip())
-    text = re.sub(r"```$", "", text.strip())
-    return text.strip()
+def recognize_text(image_path):
+    os.environ["HTTP_PROXY"] = "http://dZAYaxg6:pKSC24ap@91.220.90.245:62786"
 
-
-def gpt_recognize_check(image_path):
-    client = openai.OpenAI(api_key=settings.OPENAI_API_KEY)
     base64_image = encode_image(image_path)
     response = client.chat.completions.create(
         model="gpt-4o",
@@ -57,6 +47,7 @@ def gpt_recognize_check(image_path):
                         "text": (
                             "Это банковский чек, полученный от клиента. "
                             "Распознай его и выведи информацию в виде json c ключами:\n"
+                                                      
                             "Сумма: amount\n"
                             "Статус: status (1 если транзакция успешна, -1 если не успешна. 0 Если не понятно\n"
                             "Дата и время: create_at\n"
@@ -67,7 +58,7 @@ def gpt_recognize_check(image_path):
                             "Если на чеке есть печать банка — скорее всего операция успешна. "
                             "На чеках Unibank, если карта получателя указана в верхнем тексте перед основной таблицей, это всё равно получатель."
                             "Если чек из банковского приложения или интерфейса, и нет явных признаков неуспеха (например, красной надписи об ошибке или отказе), считать транзакцию успешной"
-                            "На выходе должен получится чистый JSON без лишних символов: символов json в резудьтате быть не должно. amount брать модуль числа"
+                            "На выходе должен получится чистый JSON без лишних символов: символов json в резудьтате быть не должно"
                         )
                     }
                 ]
@@ -81,7 +72,7 @@ def gpt_recognize_check(image_path):
     total_tokens = usage.total_tokens
     cost_usd = total_tokens * 0.000005
 
-    logger.info(f"📄 {image_path.name}")
+    print(f"📄 {image_path.name}")
     print(response.choices[0].message.content)
     print(f"🔢 Входные токены: {input_tokens}")
     print(f"🔢 Выходные токены: {output_tokens}")
@@ -89,10 +80,33 @@ def gpt_recognize_check(image_path):
     return response.choices[0].message.content
 
 
-def send_image_to_gpt(image_field, server_url="http://45.14.247.139:9000/recognize/"):  # Резерв Payment
-    with image_field.open('rb') as f:
-        files = {'file': (image_field.name, f, 'image/jpeg')}
-        with Timer("Отправляю файл на сервер..."):
-            response = requests.post(server_url, files=files, timeout=10)
+def send_image(image_path, server_url="http://45.14.247.139:9000/recognize/"):
+    image_path = Path(image_path)
+    with open(image_path, "rb") as f:
+        files = {'file': (image_path.name, f, 'image/jpeg')}
+        print("Отправляю файл на сервер...")
+        response = requests.post(server_url, files=files, timeout=16)
+    print(response.status_code)
     result = response.json().get('result')
     return result
+
+
+if __name__ == "__main__":
+    folder = Path(__file__).parent.resolve()
+    print(folder)
+    image_files = list(folder.glob("*.jpg")) + list(folder.glob("*.jpeg")) + list(folder.glob("*.png"))
+
+    print(f"🧾 Найдено файлов: {len(image_files)}")
+
+    for image_path in image_files:
+        print(image_path)
+        # try:
+        #     result = recognize_text(image_path)
+        #     print(f'result: {json.loads(result)}')
+        #
+        # except Exception as e:
+        #     print(f"Ошибка при обработке {image_path.name}: {e}")
+        # break
+        result = send_image(image_path)
+        print(result)
+
