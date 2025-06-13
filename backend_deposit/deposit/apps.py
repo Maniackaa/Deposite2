d955204ab188fb159aa6cde8d68be604b443fcec
Maniackaa <maniac_kaa@mail.ru>
2025-06-13 21:51:43 +0700

    fix incoming check

--- backend_deposit/deposit/apps.py
+++ backend_deposit/deposit/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DepositConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'deposit'
+
+    def ready(self):
+        import deposit.models
